99726801c44d5ad280f75326184c95cd488b4000
LanitaJ <55699666+LanitaJ@users.noreply.github.com>
2020-10-11 16:32:24 +0300

    find_shortest_path done

--- deadlocks.c
+++ deadlocks.c
@@ -70,7 +70,14 @@ t_room **shortest_path(t_lemin *lem)
     //int head = 0;    Индекс первого элемента.
     //int tail = 0;    Индекс элемента, следующего за последним.
 
-void push(int *tail, t_room **a, t_room *x)
+int		isolated(t_room *room)
+{
+	if (room->num_links == 0)
+		return (1);
+	return (0);
+}
+
+void	push(int *tail, t_room **a, t_room *x)
 {
 	a[*tail] = x;
 	*tail += 1;
@@ -94,8 +101,13 @@ int is_empty(int head, int tail)
 
 static	void init_bfs(t_lemin *lem, int *head, int *tail)
 {
+	int	i;
+
+	i = 0;
 	*head = 0;
 	*tail = 0;
+	while (i < lem->num_rooms)
+		lem->rooms[i++].depth = -1;
 	lem->start_room->depth = 0;
 	lem->start_room->visited = 1;
 	lem->end_room->depth = 2147483647;
@@ -117,27 +129,27 @@ void print_queue(t_room **queue, t_lemin *lem)
 
 t_room	**move_back(t_lemin *lem)
 {
-	t_room	*cur;
+	t_room	*room;
 	t_room	**sh; //shortest path
 	int		i;
 	int		ins;
 
-	ins = lem->end_room->depth - 1;
-	cur = lem->end_room;
+	ins = lem->end_room->depth;
+	room = lem->end_room;
+	//кратчайший путь имеет длину = глубине конечной комнаты
 	if ((sh = (t_room**)ft_memalloc(sizeof(t_room*) * (lem->end_room->depth + 1))) == NULL)
-		exit(1);//кратчайший путь имеет длину = глубине конечной комнаты
-	sh[ins + 1] = lem->end_room;
-	ft_printf("%d", ft_strequ(cur->name, lem->start_room->name));//некорректно сравнивает
-																//узнать как применить хэшмап
-	while (ft_strequ(cur->name, lem->start_room->name))
+		exit(1);
+	//sh[lem->end_room->depth] = lem->end_room;
+	while (ft_strequ(room->name, lem->start_room->name) == 0)//1 = rooms are equal
 	{
 		i = 0;
-		while (cur->n_rooms[i]->depth != cur->depth - 1 && i < cur->num_links)
+		while (room->n_rooms[i]->depth != room->depth - 1 && i < room->num_links)
 			i++;
-		sh[ins] = cur;
+		sh[ins] = room;
 		ins--;
-		cur = cur->n_rooms[i];
+		room = room->n_rooms[i];
 	}
+	sh[ins] = lem->start_room;
 	return(sh);
 }
 
@@ -156,9 +168,9 @@ int		bfs(t_lemin *lem)
 	push(&tail, queue, lem->start_room);
 	while (!is_empty(head, tail))
 	{
-		int k = 0;
+		//int k = 0;
 		node = pop(&head, &tail, queue);
-		ft_printf("current node %s\nlinks: ", node->name);
+		/* ft_printf("current node %s\nlinks: ", node->name);
 		while (k < node->num_links)
 		{
 			ft_printf("%s ", node->n_rooms[k]->name);
@@ -169,13 +181,13 @@ int		bfs(t_lemin *lem)
 		{
 			ft_printf("end\n");
 			return (1);
-		}	
+		} */	
 		i = -1;
 		while (++i < node->num_links)
 			if (node->n_rooms[i]->visited == 0)
 			{
-				ft_printf("name = %s\n", node->n_rooms[i]->name);
-				ft_printf("numlink %d\n", node->num_links);
+				//ft_printf("name = %s\n", node->n_rooms[i]->name);
+				//ft_printf("numlink %d\n", node->num_links);
 				push(&tail, queue, node->n_rooms[i]);
 				node->n_rooms[i]->visited = 1;
 				node->n_rooms[i]->depth = node->depth + 1;
@@ -196,7 +208,7 @@ void		find_pathes(t_lemin *lem)
 	bfs(lem);
 	while (i < lem->num_rooms)
 	{
-		ft_printf("%s %d\n",lem->rooms[i].name, lem->rooms[i].depth);
+		ft_printf("name: %s depth: %d\n",lem->rooms[i].name, lem->rooms[i].depth);
 		i++;
 	}
 	i = 0;
@@ -204,52 +216,9 @@ void		find_pathes(t_lemin *lem)
 	while (i <= lem->end_room->depth)
 	{
 		ft_printf("%s-", sh[i]->name);
-	}
-	
-	//ft_printf("dep %d", lem->start_room->n_rooms[0]->n_rooms[0]->depth);
-	/********Для тестирования*********/
-	/* 
-	t_room **room_arr;
-	t_room *poped;
-	int head = 0;
-	int tail = 0;	
-	if ((room_arr = (t_room**)ft_memalloc(sizeof(t_room*) * lem->num_rooms)) == NULL)
-		exit(1);
-	push(&tail, room_arr, lem->end_room);
-	ft_printf("%s\n", room_arr[0]->name);
-	ft_printf("%d\n%d\n%d\n", is_empty(head, tail), head, tail);
-	poped = pop(&head, &tail, room_arr);
-	ft_printf("%s", poped->name);
-	 */
-	/********Для тестирования*********/
-	
-	//room_arr = 
-	/* 
-	поправить bfs чтобы граф ex2_1.map считал вершины так: 0 1 2 4 3
-	сделать, чтобы bfs работал до тех пор, пока не дойдет до узла end 
-		или пока не кончатся комнаты или пока не кончатся комнаты в очереди
-	*/
-	/* int i = 0;
-	while (i < lem->num_rooms)
-	{
-		ft_printf("%s ", room_arr[i]->name);
 		i++;
-	} */
+	}
 }
-
-
-
-/* 
-
-
-
-(1)BFS расставляет глубину для каждого узла
-(2)После BFS идем с конца в начало и ищем кратчайший путь и запоминаем его
-(3)записываем путь в массив
-(4)разворачиваем ребра
-   goto (1)
-*/
-
 
 /* BFS(start_node, goal_node)
 {	
